f71ef047fd2b19306e4212f1193a179074d29a67
Shunping Xie <sxie12@illinois.edu>
2015-09-05 20:11:42 -0500

    updated projects

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // This is the JavaScript file
+
+$(window).scroll(function() {
+    var topOff = $(window).scrollTop();
+    var opacityLevel = Math.max(0.6, 1 - topOff/800);
+    $('.desktop_header_table').css("opacity", opacityLevel);
+    $('.header').css("opacity", opacityLevel);
+});
+
+var hover = $('.header').hover(function() {
+    $('.desktop_header_table').css("opacity", 1);
+    $('.header').css("opacity", 1);
+});
+
+
+$(document).ready(hover);
